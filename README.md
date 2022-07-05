04fa1a7e775596b3aeca2ce9356a74e16cd4189f
tonyyuan <tyuan@suse.com>
2022-07-05 10:19:32 +0800

    Update README and improve code

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # jade-tool
-The tool compares two build test results for a openQA job group and looks for improvements and regressions. It retrieves all failed tests of two builds via OpenQA rest API and compare their flavor, arc, test, module, machine and test result.The comparison result will be shown in a tree table on the web page.
+The tool is written in javascript and executed with nodejs. It compares two build test results for a openQA job group and looks for improvements and regressions. It retrieves all failed tests of two builds via OpenQA rest API and compare their flavor, arc, test, module, machine and test result.The comparison result will be shown in a tree table on the web page.
+
+Required modules:
+request-promise, express, debug
+
+# i.e. npm install express
 
 How to use
 
@@ -7,3 +12,17 @@ How to use
 2. jade-tool # node jade-express.js
 3. Open http://ip:8088/jade.html with your web browser
 
+
+The diff table:
+
+The dark red color round icon: known failure, which means that test module failed in both builds
+The red: new failure which means test module passed in previous build but failed in current build (later build). regressions could be triggered.
+The green: improvement which means test module passed in current build but failed in previous build.
+The whole table is actually a union for all failed test of the two builds. dark + red are the all of failed test for current build. dark + green are the all for previous build.
+
+The "Show difference only" checkbox will filter away dark red, which only show improvements and regressions.
+
+Noted: a test module which failed in previous build but was not executed or cancelled (no test result) in current build is also marked green.
+
+
+tyuan@suse.com
--- jade-express.js
+++ jade-express.js
@@ -4,55 +4,52 @@
 //
 //curl  -s -H "Accept: application/json" "https://openqa.suse.de/api/v1/jobs?groupid=265&build=148.1&latest=1&version=15-SP3"|jq '.jobs|[group_by(.settings.ARCH)|.[]|{(.[0].settings.ARCH): (group_by(.settings.MACHINE)|sort_by(length)|.[-1]|.[0].settings.MACHINE)}]|add'
 //
-//The commands above are used for debuging and testing.
-//
-var qString = { latest: 1, scope: "current", result: "failed" };
-var openqa_url = "https://openqa.suse.de/api/v1/jobs";
-var openqa_opensuse_url = "https://openqa.opensuse.org/api/v1/jobs";
-var express = require("express");
+var qString = {latest: 1, scope: "current", result: "failed"};
+var openqa_url = 'https://openqa.suse.de/api/v1/jobs';
+var openqa_opensuse_url = 'https://openqa.opensuse.org/api/v1/jobs';
+var express = require('express');
 var app = express();
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", function (req, res) {
-    res.send("Hello World");
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+app.get('/json', function (req, res) {
+   console.log("Got a " + JSON.stringify(req.query));
+	if (req.query.distri == "opensuse") {openqa_url = openqa_opensuse_url;}
+const request = require('request-promise');
+const {lastBuild, firstBuild, ...qstr} = {...req.query, ...qString};
+	console.log(qstr);
+debugger;
+
+let build_ids= [req.query.firstBuild.trim(), req.query.lastBuild.trim()].map(id => {let option = {url: openqa_url, qs: {build: id, ...qstr}, rejectUnauthorized: false};  
+debugger;
+	return option; });
+	const promises = build_ids.map(url => request(url));
+Promise.all(promises).then((data) => {
+	debugger;
+	let a01 = JSON.parse(data[0]);
+	let a02 = JSON.parse(data[1]);
+	if (a01.jobs.length === 0) {
+          res.status(404).send(req.query.firstBuild + ": empty search result");
+	} else if ( a02.jobs.length === 0) {
+          res.status(404).send(req.query.lastBuild + ": empty search result");
+	} else {
+//	console.log(a01.jobs[1].test);
+   res.send(JSON.stringify([a01, a02]));
+	}	
+})
+.catch(err => {
+   console.log(err);
+
 });
 
-app.get("/json", function (req, res) {
-    console.log("Got a " + JSON.stringify(req.query));
-    if (req.query.distri == "opensuse") {
-        openqa_url = openqa_opensuse_url;
-    }
-    const request = require("request-promise");
-    //const urls = ["https://openqa.suse.de/api/v1/jobs?groupid=308&build=20210101-1&latest=1&scope=current&result=failed", "https://openqa.suse.de/api/v1/jobs?groupid=308&build=20210101-1&latest=1&scope=current&result=failed"];
-    const { lastBuild, firstBuild, ...qstr } = { ...req.query, ...qString };
-    console.log(qstr);
-
-    let build_ids = [req.query.firstBuild.trim(), req.query.lastBuild.trim()].map((id) => {
-        let option = { url: openqa_url, qs: { build: id, ...qstr }, rejectUnauthorized: false };
-        return option;
-    });
-    const promises = build_ids.map((url) => request(url));
-    Promise.all(promises)
-        .then((data) => {
-            let a01 = JSON.parse(data[0]);
-            let a02 = JSON.parse(data[1]);
-            if (a01.jobs.length === 0) {
-                res.status(404).send(req.query.firstBuild + ": has no failed test or no data. Please give another build ID or check if other inputs are correct.");
-            } else if (a02.jobs.length === 0) {
-                res.status(404).send(req.query.lastBuild + ": has no failed test or no data. Please give another build ID or check if other inputs are correct.");
-            } else {
-                //	console.log(a03.jobs[1].test);
-                res.send(JSON.stringify([a01, a02]));
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
+})
 
 var server = app.listen(8088, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("App listening at http://%s:%s", host, port);
-});
-
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
